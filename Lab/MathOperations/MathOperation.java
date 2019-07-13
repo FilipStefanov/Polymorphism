d9faa385a29f2876a7ba6ac8fdd4072e9f9a39c2
Filip Stefanov <f.stefanov@yahoo.com>
2019-07-13 10:57:57 +0300

    Update MathOperation.java

--- Lab/MathOperations/MathOperation.java
+++ Lab/MathOperations/MathOperation.java
@@ -2,13 +2,28 @@ package Lab.MathOperations;
 
 public class MathOperation {
 
-    public int add( int a, int b){
-        return a+b;
-    }
-    public int add( int a, int b, int c){
-        return a+b+c;
-    }
-    public int add( int a, int b, int c, int d){
-        return a+b+c+d;
+//    public int add( int a, int b){
+//        return a+b;
+//    }
+//    public int add( int a, int b, int c){
+//        return a+b+c;
+//    }
+//    public int add( int a, int b, int c, int d){
+//        return a+b+c+d;
+//    }
+
+
+    public int add(int... element){
+        int sum = 0;
+
+        if (element.length == 2){
+            return element[0]+element[1];
+        }else
+
+            for (int i : element) {
+                sum+=i;
+            }
+
+        return sum;
     }
 }
